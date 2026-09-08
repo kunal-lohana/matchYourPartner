4a7e211463a4873cd5ed43a091d73fa10d831ca5
kunal-lohana <kunal.lohana84@gmail.com>
2026-09-08 11:36:18 +0530

    practice of get,post,put,patch,delete http methods

--- src/app.js
+++ src/app.js
@@ -2,12 +2,37 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-    res.end("/hello api is running on 7777");
-});
+// GET call for user
+app.get("/user", (req, res) => {
+    res.end("Fetch User data successfully !");
+})
 
-app.use("/test", (req, res) => {
-    res.end("/test api is running...");
+
+// POST call for user
+app.get("/user", (req, res) => {
+    res.end("Fetch User data successfully !");
+})
+
+// POST call for user
+app.post("/user", (req, res) => {
+    const data = { firstname: 'kunal', lastname: 'lohana'};
+    console.log(data);
+    res.send(data);
+})
+
+// PUT call for user
+app.put("/user", (req, res) => {
+    res.end("Update User data successfully !");
+})
+
+// PATCH call for user
+app.patch("/user", (req, res) => {
+    res.end("Update partially User data successfully !");
+})
+
+// DELETE call for user
+app.delete("/user", (req, res) => {
+    res.end("Delete User data successfully !");
 })
 
 
