ad34f87bd628566abd569c3b9d13fff5993fdb72
kunal-lohana <kunal.lohana84@gmail.com>
2026-09-12 08:32:06 +0530

    usage of request query and params

--- src/app.js
+++ src/app.js
@@ -3,39 +3,28 @@ const express = require("express");
 const app = express();
 
 // GET call for user
-app.get("/user", (req, res) => {
-    res.end("Fetch User data successfully !");
-})
-
-
-// POST call for user
-app.get("/user", (req, res) => {
-    res.end("Fetch User data successfully !");
-})
-
-// POST call for user
-app.post("/user", (req, res) => {
-    const data = { firstname: 'kunal', lastname: 'lohana'};
-    console.log(data);
-    res.send(data);
-})
-
-// PUT call for user
-app.put("/user", (req, res) => {
-    res.end("Update User data successfully !");
-})
-
-// PATCH call for user
-app.patch("/user", (req, res) => {
-    res.end("Update partially User data successfully !");
-})
-
-// DELETE call for user
-app.delete("/user", (req, res) => {
-    res.end("Delete User data successfully !");
-})
-
+app.get("/user/:userId", (req, res) => {
+    const { userId } = req.query;
+    console.log(req.params);
+    console.log(req.query);
+    // res.write(req.params);
+    res.json({
+        query: req.query,
+        params: req.params
+    })
+});
 
 app.listen(7777, () => {
     console.log('Server is running on port 7777...');
 })
+
+// http://localhost:7777/user/23?firstname=kunal&lastname=lohana
+// {
+//     "query": {
+//         "firstname": "kunal",
+//         "lastname": "lohana"
+//     },
+//     "params": {
+//         "userId": "23"
+//     }
+// }
